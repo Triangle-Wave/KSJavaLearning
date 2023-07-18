90f388f7e10213c70cd0be194add9c3e3cf7472b
twave <1340281297@qq.com>
2023-07-18 10:42:57 +0800

    获取当前路径的方法

---
+++ src/main/java/org/example/ioTest/PathTest.java
@@ -0,0 +1,17 @@
+package org.example.ioTest;
+
+import java.io.File;
+
+/**
+ * @author : twave
+ * @date : 2023/7/18 10:01
+ */
+public class PathTest {
+    public static void main(String[] args) {
+        String path = PathTest.class.getResource("").getPath();
+        String parentPath = new File(path).getParent();
+        parentPath = new File(parentPath).getParent();
+        parentPath = new File(parentPath).getParent();
+        System.out.println(parentPath);
+    }
+}

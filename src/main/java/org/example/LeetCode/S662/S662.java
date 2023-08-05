93a6816b0903adc1f58573949b7aa8877c80dce9
twaveLab <1340281297@qq.com>
2023-08-05 17:13:02 +0800

    力扣第662题

---
+++ src/main/java/org/example/LeetCode/S662/S662.java
@@ -0,0 +1,45 @@
+package org.example.LeetCode.S662;
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author TWAVE
+ * @date 2023/8/5 16:57
+ */
+public class S662 {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // Pair<TreeNode Integer> 用于记录节点和节点的位置
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        // 先把根节点入队
+        queue.offer(new Pair<>(root, 0));
+        int maxWidth = 0;
+        while (!queue.isEmpty()) {
+            // 左节点的位置是当前层的第一个节点的位置
+            int left = queue.peek().getValue();
+            // 开始时右节点位置和左节点位置一致
+            int right = left;
+            for (int i = 0; i < queue.size(); i++) {
+                // 取出Pair
+                Pair<TreeNode, Integer> pair = queue.poll();
+                // 取出节点
+                TreeNode node = pair.getKey();
+                right = pair.getValue();
+                if (node.left != null) {
+                    queue.offer(new Pair<>(node.left, right * 2));
+                }
+                if (node.right != null) {
+                    queue.offer(new Pair<>(node.right, right * 2 + 1));
+                }
+            }
+            maxWidth = Math.max(maxWidth, right - left + 1);
+        }
+        return maxWidth;
+
+    }
+}
---
+++ src/main/java/org/example/LeetCode/S662/TreeNode.java
@@ -0,0 +1,27 @@
+package org.example.LeetCode.S662;
+
+/**
+ * @author TWAVE
+ * @date 2023/8/5 16:58
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+

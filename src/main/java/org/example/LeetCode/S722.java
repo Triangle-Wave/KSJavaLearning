c2825ab367aefa811030f3d65c68b366f2e3c624
twaveLab <1340281297@qq.com>
2023-08-05 15:56:47 +0800

    力扣第722题

--- src/main/java/org/example/LeetCode/S722.java
+++ src/main/java/org/example/LeetCode/S722.java
@@ -11,6 +11,9 @@ public class S722 {
 
     public List<String> removeComments(String[] source) {
         String joinedSource = String.join("\n", source);
+        // 这段正则分为两部分
+        // //.*用于匹配单行注释
+        // /\*(.|\n)*?\*/用于匹配多行注释，以/*开头，匹配任意数量的任意字符，包含换行符，直到遇到/*结束
         String withoutComments = joinedSource.replaceAll("//.*|/\\*(.|\\n)*?\\*/", "");
         List<String> result = new ArrayList<>(Arrays.asList(withoutComments.split("\n")));
         result.removeIf(String::isEmpty);

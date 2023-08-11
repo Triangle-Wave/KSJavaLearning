468bd0b2156ce03bd8d630195659cb1c8619b410
twaveLab <1340281297@qq.com>
2023-08-11 17:07:38 +0800

    add:力扣第1289题

--- src/main/java/org/example/LeetCode/S1289.java
+++ src/main/java/org/example/LeetCode/S1289.java
@@ -1,5 +1,7 @@
 package org.example.LeetCode;
 
+import java.util.Arrays;
+
 /**
  * @author TWAVE
  * @date 2023/8/10 20:41
@@ -7,35 +9,34 @@ package org.example.LeetCode;
 public class S1289 {
     public int minFallingPathSum(int[][] grid) {
         int n = grid.length;
-        int[][] d = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                d[i][j] = Integer.MAX_VALUE;
-            }
+        int[][] dp = new int[n][n];
+        for (int[] ints : dp) {
+            Arrays.fill(ints, Integer.MAX_VALUE);
         }
-        System.arraycopy(grid[0], 0, d[0], 0, n);
+        System.arraycopy(grid[0], 0, dp[0], 0, n);
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     if (j == k) {
                         continue;
                     }
-                    d[i][j] = Math.min(d[i][j], d[i - 1][k] + grid[i][j]);
+                    dp[i][j] = Math.min(dp[i][j], grid[i][j] + dp[i - 1][k]);
                 }
             }
         }
         int res = Integer.MAX_VALUE;
-        for (int j = 0; j < n; j++) {
-            res = Math.min(res, d[n - 1][j]);
+        for (int i : dp[n - 1]) {
+            res = Math.min(res, i);
         }
         return res;
     }
 
     public static void main(String[] args) {
         int[][] matrix = {
-                {2, 1, 3},
-                {6, 5, 4},
-                {7, 8, 9}
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
         };
         S1289 s1289 = new S1289();
         System.out.println(s1289.minFallingPathSum(matrix));

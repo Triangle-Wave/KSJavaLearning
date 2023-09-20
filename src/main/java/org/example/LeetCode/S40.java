b57a7c72a1be79da3b426686998f9effb7ecb8af
twaveLab <1340281297@qq.com>
2023-09-20 10:49:26 +0800

    add: LeetCode第40题

---
+++ src/main/java/org/example/LeetCode/S40.java
@@ -0,0 +1,21 @@
+package org.example.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author TWAVE
+ * @date 2023/9/19 10:51
+ */
+public class S40 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
